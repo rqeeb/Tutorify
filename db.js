c50b16d2ae2736a9bb91bed1c33127231d6ce2d4
RaqeebDev <raqeebauto@gmail.com>
2026-01-25 22:07:02 +0530

    package changes & db upgrade

--- db.js
+++ db.js
@@ -1,38 +1,37 @@
-const {Schema} = require('mongoose');
-const mongoose = require('mongoose');
-const ObjectId = mongoose.type.ObjectId;
+const { Schema } = require("mongoose");
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
-const dotenv = require('dotenv');
-dotenv.config();
+require("dotenv").config();
 const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI)
+mongoose
+  .connect(mongoURI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
 
 //Schemas
 const userSchema = new Schema({
-    email: {type: String , unique: true},
-    password: String,
-    firstName: String,
-    lastName: String,
-})
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
 const adminSchema = new Schema({
-    email: {type: String , unique: true},
-    password: String,
-    firstName: String,
-    lastName: String,
-})
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
 const courseSchema = new Schema({
-    title: String,
-    description: String,
-    imageUrl: String,
-    creatorId: mongoose.Types.ObjectId,
-})
+  title: String,
+  description: String,
+  imageUrl: String,
+  creatorId: mongoose.Types.ObjectId,
+});
 const purchaseSchema = new Schema({
-    user: ObjectId,
-    course: ObjectId,
-
-})
+  user: ObjectId,
+  course: ObjectId,
+});
 
 const userModel = mongoose.model("user", userSchema);
 const adminModel = mongoose.model("admin", adminSchema);
