883ca52bdd7790169279b30ac249e983af733d6f
RaqeebDev <raqeebauto@gmail.com>
2026-01-25 22:28:34 +0530

    added readme

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { userRouter } = require("./routes/user.js");
 const { courseRouter } = require("./routes/course.js");
 const { adminRouter } = require("./routes/admin.js");
+app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
